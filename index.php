f251dae8f4ede4154f01b801143957b860510bd6
Justin Garrison <60638240+justingarrison99@users.noreply.github.com>
2020-12-28 16:07:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Germania+One&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css"
+              href="styles.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="container">
+            <div class ="center">
+                <a href="https://open.spotify.com/artist/0Vh4YFAldI3sil8yOw6eNo?si=e9tzB_OxSYOUZ1ydnxtSpQ" target="_blank"><img id="lango1" src="./images/lango1.png"></a>
+                <a href="https://soundcloud.com/sandcastlebandofficial" target="_blank"><img src="./images/lango2.png"></a>
+                <a href="https://www.instagram.com/sandcastlebandofficial/" target="_blank"><img src="./images/lango3.png"></a>
+                <a href="https://sandcastlebandofficial.bandcamp.com/" target="_blank"><img src="./images/lango4.png"></a>
+            </div>
+        </div>
+    </body>
+</html>
